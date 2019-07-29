c51977bf7f0f59e7ea54d58ae0c40313046d553a
louis476078 <138080554@qq.com>
2019-07-30 00:53:14 +0800

    2019/7/30 新加了一道题但是还没做完整 :交换输出

---
+++ 第七章/交换输出/coveroutput.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int find_min(int *data,int length)
+{
+    if(data==NULL)
+    {
+        return 0;
+    }
+    int i,j,min,temp;
+    min=data[0];
+    temp=0;
+    for(i=0;i<length;i++)
+    {
+        if(min>data[i]);
+        {
+            temp=data[i];
+            data[i]=min;
+            min=temp;
+        }
+    }
+    return min;
+}
+int main()
+{
+    int data[100]={0};
+    int length=0;
+    int i,j;
+    i=0,j=0;
+    while(length<100)
+    {
+        scanf("%d",&data[length]);
+        
+        if(data[length]==0)
+        {
+            printf("input 0 so break\n");
+            break;
+        }
+        length++;
+    }
+    while(i<length)
+    {
+        printf("%d ",data[i]);
+        i++;
+    }
+    printf("\n");
+    int min=find_min(data,length);
+    data[0] = min;
+    while(j<length)
+    {
+        printf("%d ",data[j]);
+        j++;
+    }
+    printf("\n");
+    return 0;
+}
